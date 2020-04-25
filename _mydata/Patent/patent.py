f0a980e8bf83e3c26d773b7a8247d8c588c3a790
yh2424@gmail.com <2481fjqm>
2020-04-25 12:06:42 +0200

    MOD - patent

--- _mydata/Patent/patent.py
+++ _mydata/Patent/patent.py
@@ -3,10 +3,19 @@ import os
 import sys
 
 
-# print (os.path.join(sys.path[0], "my_file.txt"))
-df = pd.read_excel(r"C:\Users\kim96\PycharmProjects\yh2424.github.io\_mydata\Patent\20200425171408004.xls")
+# http://abpat.kipris.or.kr/
+# younghyun*takenaka
+# (young hyun)*takeya
+# http://www.freepatentsonline.com/
 
 
+# import patent list file
+df = pd.read_excel(r"C:\Users\kim96\PycharmProjects\yh2424.github.io\_mydata\Patent\patent_list_YK.xls")
 for i in range(len(df)):
     # print (i)
-    print ('- %s%s, %s, %s [[Link]]()' %(df['국가'][i], df['공보번호'][i]), df['발명의 명칭'], df['DOP'])
+
+    opt = 'B2' if df['등록번호'][i] > 0 else 'A1'
+    filename = str(df['국가'][i])+str(df['공보번호'][i])+ opt
+    link = 'https://github.com/yh2424/yh2424.github.io/blob/master/_mydata/Patent/pdf/'
+    print ('- %s, %s, %s [[Link]](https://github.com/yh2424/yh2424.github.io/raw/master/_mydata/Patent/pdf/%s.pdf)    ' %(filename, df['발명의 명칭'][i], df['DOP'][i], filename))
+
